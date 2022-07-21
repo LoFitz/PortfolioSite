70c26dd724eb8bd55f2ab1869285b2a31324f265
Lauren <44005289+LoFitz@users.noreply.github.com>
2022-07-21 15:40:10 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,7 @@ const Header = props => (
       <div className="inner">
         <h1>Dimension</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Hello welcome to my website!
         </p>
       </div>
     </div>
@@ -26,7 +22,7 @@ const Header = props => (
               props.onOpenArticle('intro')
             }}
           >
-            Intro
+            Farts
           </button>
         </li>
         <li>
